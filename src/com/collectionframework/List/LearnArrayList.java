a1764c926be88ca448486572d9742d3029f86e33
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-26 09:26:58 +0530

    ArrayList

--- src/com/collectionframework/List/LearnArrayList.java
+++ src/com/collectionframework/List/LearnArrayList.java
@@ -1,2 +1,24 @@
-package com.collectionframework.List;public class LearnArrayList {
+package com.collectionframework.List;
+
+import java.util.ArrayList;
+
+public class LearnArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> aList = new ArrayList<>();
+        aList.add(12);
+        aList.add(11);
+        aList.add(12);
+        aList.add(11);
+        aList.add(14);
+
+        /*
+         List contains duplicate elements
+         ArrayList doesn't have fixed size i.e. it dynamic in nature
+         */
+
+        System.out.println(aList);
+    }
+
 }
