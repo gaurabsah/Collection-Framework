c7f28a89f32d8bc8290d13b180f1cfdb899b820d
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-26 09:43:22 +0530

    methods of ArrayList

--- src/com/collectionframework/List/LearnArrayList.java
+++ src/com/collectionframework/List/LearnArrayList.java
@@ -1,6 +1,9 @@
 package com.collectionframework.List;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class LearnArrayList {
 
@@ -19,6 +22,37 @@ public class LearnArrayList {
          */
 
         System.out.println(aList);
+
+
+        /*
+        you can declare ArrayList in another way as well
+        Here List is a interface and ArrayList is a class
+         */
+
+        List<String> list = new ArrayList<>();
+        list.add("Gaurab");
+        list.add("Saurab");
+        list.add("Gaurab");
+        list.add("Ram");
+        list.add("Sita");
+        list.add("Shyam");
+
+        System.out.println(list);
+
+        list.add(0,"adding element at 0th index");
+        System.out.println(list);
+
+        List<String> l =list.stream().distinct().collect(Collectors.toList());
+        System.out.println(l); // it will remove duplicate element present in list
+
+        list.remove(0);
+        System.out.println(list); // this will remove the element at index 0
+
+        boolean bool = list.contains("Gaurab");
+        System.out.println(bool);   // this will check if the list contains the following element or not
+
+        list.clear();
+        System.out.println(list); // this will clear/empty the list
     }
 
 }
